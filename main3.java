d2433b0cb2c51e6f4c29ccfc6fcdc2a1bead816a
jerryW1204 <k26gjk05@st.kyusan-u.ac.jp>
2026-06-08 10:59:01 +0900

    0525

---
+++ main3.java
@@ -0,0 +1,11 @@
+public class main3 {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        pi*=2;
+        System.out.println(2*2*pi);
+
+        for(String s: args){
+            System.out.println(s);
+        }
+    }
+}
