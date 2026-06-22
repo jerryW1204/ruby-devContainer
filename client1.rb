857317e24cd960858143b48be4d046e2cf4762bb
jerryW1204 <k26gjk05@st.kyusan-u.ac.jp>
2026-06-22 12:01:21 +0900

    HTTP/1.1

--- client1.rb
+++ client1.rb
@@ -7,10 +7,23 @@ host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
 path = '/~toshi/'
 
+# host1 = 'Host: ' + host + "\r\n\r\n" # v1
 sock = TCPSocket.new host, port
-cmd = 'GET ' + path + " HTTP/1.0\r\n\r\n"
+# cmd = 'GET ' + path + " HTTP/1.1\r\n" + host1 # v1
+# cmd = 'GET ' + path + " HTTP/1.1\r\n" # v2
+# cmd = "GET #{path} HTTP/1.1\r\nHost: #{host}\r\n\r\n" # v3
+# cmd = "GET #{path} HTTP/1.1\r\nHost: #{host}\r\nConnection: close\r\n\r\n" # v3_1解决进程等待request的现象
+# cmd2 = 'Host: ' + host + "\r\n" # v2
+# cmd_end = "\r\n" # v2
+cmd = "GET #{path} HTTP/1.1\r\n" # v4_1
+cmd += "Host: #{host}\r\n"  # v4_1
+cmd += "Connection: close\r\n" # v4_1
+cmd += "\r\n" # v4_1
+
 pp cmd
 sock.print cmd
+# sock.print cmd2 # v2
+# sock.print cmd_end # v2
 
 # is_body = false
 # while line = sock.gets
