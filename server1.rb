1feb4270aedcd8321aca28b615141afb4cdf141d
jerryW1204 <k26gjk05@st.kyusan-u.ac.jp>
2026-07-06 11:40:24 +0900

    path的处理: 查看path

--- server1.rb
+++ server1.rb
@@ -4,10 +4,14 @@
 require 'socket'
 
 def server s
-  while line = s.gets
-    pp line
-    s.puts line
-    break if line == "\r\n"
+  # pp s.gets
+  cmd, path, ver = s.gets.split " " # 基于空格分割字符
+  if path == "/"
+    pp "INDEX" 
+    s.puts "index"
+  else
+    pp "OTHER"
+    s.puts "other"
   end
   s.close
 end
@@ -18,7 +22,7 @@ loop do
   s = gs.accept
   Thread.new do
     server s
-    sleep 10 # 10s
+    # sleep 10 # 10s
   end
 end
 
